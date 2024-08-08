1a3be6454ce4af36caa3c37c754ec2ed32cc2a2a
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-08 09:02:46 -0500

    Update stay.js

--- stay.js
+++ stay.js
@@ -1,1 +1,7 @@
-
+setInterval(()=>{
+ if(location.host!='www.codewars.com'){return}
+         document.body.style.filter='hue-rotate(50deg)';
+                document.querySelectorAll('[data-controller="solution-group"]:has([href*="JavaScripts"].font-semibold) [data-label="clever"]:not(.is-voted)').forEach(x=>x.click());
+                document.querySelectorAll('[data-controller="solution-group"]:has([href*="TypeScripts"].font-semibold) [data-label="clever"]:not(.is-voted)').forEach(x=>x.click());
+                document.querySelectorAll('.vote-assessment>li[data-value="1"]:not(.is-active) a.btn').forEach(x=>x.click());
+},1000);
