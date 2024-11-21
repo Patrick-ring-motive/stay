62b9f228fa7ba168d7af6ff869586d7fa6456160
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-11-20 20:51:02 -0600

    Update stay.js

--- stay.js
+++ stay.js
@@ -13,10 +13,10 @@ const wiki=setInterval(()=>{try{
  
 const CodeWars = setInterval(()=>{
  if(location.host!='www.codewars.com'){return;}
-         document.body.style.filter='hue-rotate(50deg)';
-                document.querySelectorAll('[data-controller="solution-group"]:has([href*="JavaScripts"].font-semibold) [data-label="clever"]:not(.is-voted)').forEach(x=>x.click());
-                document.querySelectorAll('[data-controller="solution-group"]:has([href*="TypeScripts"].font-semibold) [data-label="clever"]:not(.is-voted)').forEach(x=>x.click());
-                document.querySelectorAll('.vote-assessment>li[data-value="1"]:not(.is-active) a.btn').forEach(x=>x.click());
+         (document?.body?.style??{}).filter='hue-rotate(50deg)';
+         [...document.querySelectorAll('[data-controller="solution-group"]:has([href*="JavaScripts"].font-semibold) [data-label="clever"]:not(.is-voted)')].forEach(x=>x.click());
+         [...document.querySelectorAll('[data-controller="solution-group"]:has([href*="TypeScripts"].font-semibold) [data-label="clever"]:not(.is-voted)')].forEach(x=>x.click());
+         [...document.querySelectorAll('.vote-assessment>li[data-value="1"]:not(.is-active) a.btn')].forEach(x=>x.click());
 },1000);
 
 const googleAds = setInterval(()=>{
