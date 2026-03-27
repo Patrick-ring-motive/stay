c67ba4fd8fcd98933e7184efa1e3ddd94010d921
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-27 10:52:47 -0500

    style: js-beautify formatting

--- stay.js
+++ stay.js
@@ -1,28 +1,34 @@
+setInterval(() => {
+  if (`${document?.title}`.includes('👾')) {
+    return;
+  }
+  (document ?? {}).title = `👾 ${document?.title}`;
+}, 100);
 
-setInterval(()=>{
-  if(`${document?.title}`.includes('👾')){return;}
-  (document??{}).title = `👾 ${document?.title}`;
-},100);
+const wiki = setInterval(() => {
+  try {
+    if ((!~location.host.search(/wiki|pedia|fandom/i)) && (window == window.top)) {
+      return;
+    }
+    [...document.querySelectorAll('video,audio,frame,iframe,object,embed')].forEach(x => x.remove());
+    hideSelection('video,audio,frame,iframe,object,embed');
+  } catch (e) {}
+}, 100);
 
-const wiki=setInterval(()=>{try{
-  if((!~location.host.search(/wiki|pedia|fandom/i)) && (window == window.top)){return;}
-  [...document.querySelectorAll('video,audio,frame,iframe,object,embed')].forEach(x=>x.remove());
-  hideSelection('video,audio,frame,iframe,object,embed');
-}catch(e){}},100);
+const CodeWars = setInterval(() => {
+  if (location.host != 'www.codewars.com') {
+    return;
+  }
+  (document?.body?.style ?? {}).filter = 'hue-rotate(50deg)';
+  [...document.querySelectorAll('[data-controller="solution-group"]:has([href*="JavaScripts"].font-semibold) [data-label="clever"]:not(.is-voted)')].forEach(x => x.click());
+  [...document.querySelectorAll('[data-controller="solution-group"]:has([href*="TypeScripts"].font-semibold) [data-label="clever"]:not(.is-voted)')].forEach(x => x.click());
+  [...document.querySelectorAll('.vote-assessment>li[data-value="1"]:not(.is-active) a.btn')].forEach(x => x.click());
+}, 1000);
 
- 
-const CodeWars = setInterval(()=>{
- if(location.host!='www.codewars.com'){return;}
-         (document?.body?.style??{}).filter='hue-rotate(50deg)';
-         [...document.querySelectorAll('[data-controller="solution-group"]:has([href*="JavaScripts"].font-semibold) [data-label="clever"]:not(.is-voted)')].forEach(x=>x.click());
-         [...document.querySelectorAll('[data-controller="solution-group"]:has([href*="TypeScripts"].font-semibold) [data-label="clever"]:not(.is-voted)')].forEach(x=>x.click());
-         [...document.querySelectorAll('.vote-assessment>li[data-value="1"]:not(.is-active) a.btn')].forEach(x=>x.click());
-},1000);
-
-const googleAds = setInterval(()=>{
-    let selectors = `[id*="google_ads"],
+const googleAds = setInterval(() => {
+  let selectors = `[id*="google_ads"],
     [class*="google_ads"]`;
-    if(!document.querySelector(`[googleads4style]`)){
+  if (!document.querySelector(`[googleads4style]`)) {
     let s = document.createElement('style');
     s.innerText = `
     ${selectors}
@@ -32,18 +38,19 @@ const googleAds = setInterval(()=>{
       opacity:0;
     }
     `;
-    s.setAttribute(`googleads4style`,true);
+    s.setAttribute(`googleads4style`, true);
     document.body.appendChild(s);
   }
-  [...document.querySelectorAll(selectors)].forEach(x=>x?.remove?.());
-  
-},100);
+  [...document.querySelectorAll(selectors)].forEach(x => x?.remove?.());
 
+}, 100);
 
-function hideSelection(name,selectors){
-  if(!~location.host.search(name)){return;}
-  const sname = name.replaceAll('.','');
-  if(!document.querySelector(`[${sname}4style]`)){
+function hideSelection(name, selectors) {
+  if (!~location.host.search(name)) {
+    return;
+  }
+  const sname = name.replaceAll('.', '');
+  if (!document.querySelector(`[${sname}4style]`)) {
     let s = document.createElement('style');
     s.innerText = `
     ${selectors}
@@ -53,27 +60,29 @@ function hideSelection(name,selectors){
       opacity:0;
     }
     `;
-    s.setAttribute(`${sname}4style`,true);
+    s.setAttribute(`${sname}4style`, true);
     document.body.appendChild(s);
   }
-  [...document.querySelectorAll(selectors)].forEach(x=>x?.remove?.());
+  [...document.querySelectorAll(selectors)].forEach(x => x?.remove?.());
 }
 
-const GeeksForGeeks=setInterval(()=>{
-   hideSelection('www.geeksforgeeks.org',`.cookie-text,
+const GeeksForGeeks = setInterval(() => {
+  hideSelection('www.geeksforgeeks.org', `.cookie-text,
     [class*="avp"],
     [class*="commercial"],
     video,
     [id*="google_ads"],
     .widget-area`);
-},100);
+}, 100);
 
-const Medium=setInterval(()=>{
-  if(location.host!='medium.com'){return;}
+const Medium = setInterval(() => {
+  if (location.host != 'medium.com') {
+    return;
+  }
   document?.querySelector?.('[data-testid="close-button"]')?.click?.();
-},100);
+}, 100);
 
-const w3schools=setInterval(()=>{
-     hideSelection('www.w3schools.com',`[id*="adngin"],
+const w3schools = setInterval(() => {
+  hideSelection('www.w3schools.com', `[id*="adngin"],
      .sidesection`);
-},100);
+}, 100);
